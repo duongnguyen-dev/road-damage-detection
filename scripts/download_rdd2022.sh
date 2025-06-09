19a446c338b3d1c310b0dbc6785b9666dd069c94
duongng2911 <21070546@vnu.edu.vn>
2025-06-09 13:01:13 +0700

    minor fix downloading script

--- scripts/download_rdd2022.sh
+++ scripts/download_rdd2022.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+# Get the directory where the script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PROJECT_ROOT="$(dirname "$SCRIPT_DIR")"
+
 # Create data directory if it doesn't exist
-mkdir -p data
+mkdir -p "$PROJECT_ROOT/data"
 
 # Run script to download dataset
-python src/datasets/download_dataset.py --path data/rdd2022
+python "$PROJECT_ROOT/src/datasets/download_dataset.py" --path "$PROJECT_ROOT/data/rdd2022"
 
 # Check the result
 if [ $? -eq 0 ]; then
-    echo "Dataset RDD2022 was install successfully to data/rdd2022"
+    echo "Dataset RDD2022 was install successfully to $PROJECT_ROOT/data/rdd2022"
 else
     echo "There was an error while installing dataset RDD2022"
     exit 1
