23345032f6c191ceda09ed64e85b5128a10cae23
duongng2911 <21070546@vnu.edu.vn>
2025-06-09 13:42:23 +0700

    minor fix downloading script

--- src/datasets/download_dataset.py
+++ src/datasets/download_dataset.py
@@ -4,7 +4,8 @@ from src.datasets.utils import download_dataset
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Download RDD2022 dataset to specified path")
     parser.add_argument("--dataset", type=str, default="RDD2022", help="Name of the dataset to download")
+    parser.add_argument("--country", type=str, required=True, help="Name of the dataset country, the value should be either 'japan', 'india', 'czech', 'norway', 'usa', 'china_drone' or 'china_motorbike'.")
     parser.add_argument("--path", type=str, required=True, help="Path to save the dataset")
     
     args = parser.parse_args()
-    download_dataset(args.dataset, args.path)
+    download_dataset(args.dataset, args.country, args.path)
--- src/datasets/utils.py
+++ src/datasets/utils.py
@@ -1,16 +1,33 @@
 import os
-import dataset_tools as dtools
+import requests
+import zipfile
+import io
 from loguru import logger
 
-def download_dataset(dataset_name: str, dataset_path: str):
+country_link = {
+    "japan": "https://bigdatacup.s3.ap-northeast-1.amazonaws.com/2022/CRDDC2022/RDD2022/Country_Specific_Data_CRDDC2022/RDD2022_Japan.zip",
+    "india": "https://bigdatacup.s3.ap-northeast-1.amazonaws.com/2022/CRDDC2022/RDD2022/Country_Specific_Data_CRDDC2022/RDD2022_India.zip",
+    "czech": "https://bigdatacup.s3.ap-northeast-1.amazonaws.com/2022/CRDDC2022/RDD2022/Country_Specific_Data_CRDDC2022/RDD2022_Czech.zip",
+    "norway": "https://bigdatacup.s3.ap-northeast-1.amazonaws.com/2022/CRDDC2022/RDD2022/Country_Specific_Data_CRDDC2022/RDD2022_Norway.zip",
+    "usa": "https://bigdatacup.s3.ap-northeast-1.amazonaws.com/2022/CRDDC2022/RDD2022/Country_Specific_Data_CRDDC2022/RDD2022_United_States.zip",
+    "china_motorbike": "https://bigdatacup.s3.ap-northeast-1.amazonaws.com/2022/CRDDC2022/RDD2022/Country_Specific_Data_CRDDC2022/RDD2022_China_MotorBike.zip",
+    "china_drone": "https://bigdatacup.s3.ap-northeast-1.amazonaws.com/2022/CRDDC2022/RDD2022/Country_Specific_Data_CRDDC2022/RDD2022_China_Drone.zip" 
+}
+
+def download_dataset(dataset_name: str, country_name: str, dataset_path: str):
     if not os.path.exists(dataset_path):
-        logger.info(f"The provided path {dataset_path} does not exist. Creating directory {dataset_path}")
+        logger.info(f"The provided path {dataset_path} does not exist. Creating directory {dataset_path}.")
         os.makedirs(dataset_path) 
 
     try:  
-        logger.info(f"Downloading dataset {dataset_name} to {dataset_path}")
-        dtools.download_dataset(dataset_name, dataset_path) 
-        logger.info(f"Dataset {dataset_name} downloaded successfully to {dataset_path}")
+        logger.info(f"Downloading dataset {dataset_name} {country_name} to {dataset_path}.")
+        r = requests.get(country_link[country_name])
+        if r.status_code == 200:
+            z = zipfile.ZipFile(io.BytesIO(r.content))
+            z.extractall(dataset_path)
+            logger.info(f"Dataset {dataset_name} {country_name} was downloaded successfully to {dataset_path}.")
+        else:
+            logger.error(f"Error while downloading dataset {dataset_name} {country_name} to {dataset_path}: {r.status_code}.")
     except Exception as e:
-        logger.error(f"Error downloading dataset {dataset_name} to {dataset_path}: {e}")
+        logger.error(f"Error while downloading dataset {dataset_name} {country_name} to {dataset_path}: {e}.")
         raise e
