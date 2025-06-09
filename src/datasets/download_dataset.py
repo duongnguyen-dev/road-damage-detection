675c2716b2ae276184aa19843585217c9249758b
duongng2911 <21070546@vnu.edu.vn>
2025-06-09 13:51:31 +0700

    rename working folder

--- src/datasets/download_dataset.py
+++ src/datasets/download_dataset.py
@@ -1,5 +1,5 @@
 import argparse
-from road_damage_detection.datasets.utils import download_dataset
+from src.datasets.utils import download_dataset
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Download RDD2022 dataset to specified path")
